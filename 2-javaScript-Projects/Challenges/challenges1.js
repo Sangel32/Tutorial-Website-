83b1f77b08b27431f402fa3545950f3dd5c8403d
Sebastian Angel <sebastianangel@Sebastians-MacBook-Pro.local>
2019-01-10 15:33:52 -0500

    2-javascript-projects

---
+++ 2-javaScript-Projects/Challenges/challenges1.js
@@ -0,0 +1,19 @@
+//Challenge
+
+let  Palindrome = "civic";
+let  
+
+function () {
+  return ();
+}
+
+function () {
+  return ();
+}
+
+if(){
+    console.log("Word is a Palindrome");
+    else{
+    console.log("Word is not a Palindrome")
+    }
+}
